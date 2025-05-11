e54a6b3c008c9d215e94fd04b8d2765fad048780
suka <khiemnguyen.hye@gmail.com>
2025-05-11 20:10:13 +0700

    Updates

--- Week 1/ArsonStopper.js
+++ Week 1/ArsonStopper.js
@@ -1,5 +1,5 @@
 let grid = []
-let numberOfTreeToCut = 0
+let treeToCutCount = 0
 
 for (let i = 0; i < 6; i++) {
     grid.push(readline().split('').map(spot => {return spot === 'o'? '=' : spot}))
@@ -12,19 +12,72 @@ for (let x = 0; x < 6; x++) {
             // Check if the coordinates to be burnt is out-of-bound
             // Then if it's a tree
             // If it is then cut it and increase the count by one
-
-            // Upwards by one
-            if (x - 1 >= 0 && x - 1 <= 6) {
+            // UPWARDS TWICE
+            if (x - 1 >= 0 && x - 1 < 6) {
                 if (grid[x - 1][y] === '#') {
                     grid[x - 1][y] = 'X'
-                    numberOfTreeToCut++
+                    treeToCutCount++
                 }
             }
-
+            if (x - 2 >= 0 && x - 2 < 6) {
+                if (grid[x - 2][y] === '#') {
+                    grid[x - 2][y] = 'X'
+                    treeToCutCount++
+                }
+            }
+            // DOWNWARDS TWICE
+            if (x + 1 >= 0 && x + 1 < 6) {
+                if (grid[x + 1][y] === '#') {
+                    grid[x + 1][y] = 'X'
+                    treeToCutCount++
+                }
+            }
+            if (x + 2 >= 0 && x + 2 < 6) {
+                if (grid[x + 2][y] === '#') {
+                    grid[x + 2][y] = 'X'
+                    treeToCutCount++
+                }
+            }
+            // LEFTWARDS TWICE
+            if (y - 1 >= 0 && y - 1 < 6) {
+                if (grid[x][y - 1] === '#') {
+                    grid[x][y - 1] = 'X'
+                    treeToCutCount++
+                }
+            }
+            if (y - 2 >= 0 && y - 2 < 6) {
+                if (grid[x][y - 2] === '#') {
+                    grid[x][y - 2] = 'X'
+                    treeToCutCount++
+                }
+            }
+            // RIGHTWARDS TWICE
+            if (y + 1 >= 0 && y + 1 < 6) {
+                if (grid[x][y + 1] === '#') {
+                    grid[x][y - 1] = 'X'
+                    treeToCutCount++
+                }
+            }
+            if (y + 2 >= 0 && y + 2 < 6) {
+                if (grid[x][y + 2] === '#') {
+                    grid[x][y + 2] = 'X'
+                    treeToCutCount++
+                }
+            }
+            // LEFT-UPWARDS
+            if (y + 1 >= 0 && y + 1 < 6) {
+                if (grid[x][y + 1] === '#') {
+                    grid[x][y - 1] = 'X'
+                    treeToCutCount++
+                }
+            }
         }
     }
 }
 
 console.error(grid)
-
-console.log('12');
+if (treeToCutCount === 0) {
+    console.log('JUST RUN')
+    return
+}
+console.log(treeToCutCount);
