f6c53f963b9cd05d4efc986bf395c14b394fe89d
suka <khiemnguyen.hye@gmail.com>
2025-05-12 09:58:02 +0700

    Add RetroArt solution

---
+++ Week 1/RetroArt.js
@@ -0,0 +1,54 @@
+const recipe = readline().split(' ')
+
+let image = ''
+
+for (ingredient of recipe) {
+
+    // Parsing one single ingredient
+    // Either they're fully number, or isn't
+    // If it isn't fully number (4z, 7e, 88sp)
+    if (!isPureNumber(ingredient)) {
+        let tempCount = ''
+        let tempPrint = ''
+        // Parsing one single character
+        for (let character of ingredient) {
+            // If the character is a number, add it to the count
+            if (!isNaN(character)) {
+                tempCount += character
+            }
+            else {
+                tempPrint += character
+            }
+        }
+        // Parsing the meaning of the ingredient
+        if (tempPrint === 'sp') { tempPrint = ' ' }
+        else if (tempPrint === 'bS') { tempPrint = '\\' }
+        else if (tempPrint === 'sQ') { tempPrint = '\''}
+        else if (tempPrint === 'nl') { tempPrint = '\n'}
+
+        // If there is no numeric prefix, ie 'nl', set the default count to 0
+        image += tempPrint.repeat(tempCount === '' ? 1 : parseInt(tempCount));
+
+    }
+    // If it is fully a number (32, 77, 556)
+    else if (isPureNumber(ingredient)) {
+        let tempCount = 0
+        let tempPrint = ingredient
+
+        tempPrint = ingredient % 10
+        tempCount = (ingredient - tempPrint) / 10
+
+        image += String(tempPrint).repeat(tempCount)
+    }
+}
+console.log(image);
+
+function isPureNumber(ingredient) {
+    result = true
+
+    for (character of ingredient) {
+        if (isNaN(character)) { result = false }
+    }
+
+    return result
+}
