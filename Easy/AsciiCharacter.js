15e6a88684eb15825f0a53a122cb7149fb7f9981
suka <khiemnguyen.hye@gmail.com>
2025-05-19 10:41:56 +0700

    Stuff

--- Easy/AsciiCharacter.js
+++ Easy/AsciiCharacter.js
@@ -18,7 +18,7 @@ for (let i = 0; i < height; i++) {
     // for each word's first row
     let rowToPrint = ''
     for (let x = 0; x < textToPrint.length; x++) {
-        // take the index of the character within the alphabet
+        // take the index of the character within the alphabetx
         let indexOfChar = alphabet.indexOf(textToPrint[x])
         if (indexOfChar === -1) { indexOfChar = 26 }
         // slice the thing from the super alphabet and add it to the row to print
