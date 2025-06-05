cea48329db4607df7851c589d3b23cc56097a318
suka <khiemnguyen.hye@gmail.com>
2025-06-05 19:57:12 +0700

    CSS battle

---
+++ Basics/Search.py
@@ -0,0 +1,25 @@
+# this is an attempt at implementing binary search 
+import math
+
+numbers = [15, 28 , 66, 4, 3, 77, 2, 12, 1]
+
+print('before sorting:', numbers)
+
+numbers.sort()
+
+print('after sorting:', numbers)
+
+def binarySearch(target, array):
+    upperIndex = len(array) - 1
+    lowerIndex = 0
+    while (lowerIndex <= upperIndex):
+        midIndex = math.floor((upperIndex + lowerIndex) / 2)
+        if (array[midIndex] == target): 
+            return midIndex
+        elif (numbers[midIndex] > target): upperIndex = midIndex - 1
+        elif (numbers[midIndex] < target): lowerIndex = midIndex + 1
+    return -1
+
+toSearchFor = 3
+
+print(binarySearch(toSearchFor, numbers))
